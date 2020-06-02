e6df0269221369a4b59470bf22c97b0c098bfc2c
Paweł <frankowski.pawel@gmail.com>
2020-06-02 15:32:10 +0200

    Added Register and Login Page.

---
+++ src/main/java/com/sda/controller/RegisterController.java
@@ -0,0 +1,17 @@
+package com.sda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RegisterController {
+
+    @GetMapping("/register")
+    public String register(Model model){
+
+        return "/register";
+    }
+}
--- src/main/java/com/sda/controller/UserController.java
+++ src/main/java/com/sda/controller/UserController.java
@@ -14,6 +14,8 @@ public class UserController {
     @Autowired
     private UserService service;
 
+
+
     @PostMapping("/addUser")
     public User addProduct(@RequestBody User user) {
         return service.saveUser(user);
