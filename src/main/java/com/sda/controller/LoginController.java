fc6535c9c834389042f1207f55c0633b7fa4db68
Paweł <frankowski.pawel@gmail.com>
2020-06-03 22:15:04 +0200

    Created form login + controller. Not finished yet.

--- src/main/java/com/sda/controller/LoginController.java
+++ src/main/java/com/sda/controller/LoginController.java
@@ -1,16 +1,36 @@
 package com.sda.controller;
 
+import com.sda.entity.User;
+import com.sda.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/login")
-    public String home(Model model){
+    public String showForm(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String submitForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResultUser, Model model) {
+
+        System.out.println(user.getEmail()+"/"+user.getPassword());
 
         return "login";
     }
--- src/main/java/com/sda/controller/RegisterController.java
+++ src/main/java/com/sda/controller/RegisterController.java
@@ -1,22 +1,17 @@
 package com.sda.controller;
 
-import com.sda.entity.Address;
 import com.sda.entity.User;
 import com.sda.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -30,10 +25,8 @@ public class RegisterController {
     public String showForm(Model model) {
         User user = new User();
         model.addAttribute("user", user);
-
         return "register";
     }
-
 
     @PostMapping("/register")
     public String submitForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResultUser, Model model) {
@@ -50,7 +43,6 @@ public class RegisterController {
                 bindingResultUser.rejectValue("email", "error.user", "Podany email jest powiązany z innym kontem");
                 return "register";
             }
-
         }
     }
 }
